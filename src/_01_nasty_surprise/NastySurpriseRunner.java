54465ac6538e4a3e0635dd7a5e62099095a6fd79
student <student@ip-172-31-60-188.us-west-2.compute.internal>
2021-10-31 11:26:21 -0700

    yeet

---
+++ src/_01_nasty_surprise/NastySurpriseRunner.java
@@ -0,0 +1,24 @@
+package _01_nasty_surprise;
+
+import javax.swing.JButton;
+
+public class NastySurpriseRunner {
+
+	JButton Trick = new JButton();
+	JButton Treat = new JButton();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
