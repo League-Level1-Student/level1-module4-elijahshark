7285b16f547084942b9f88d45015926ca67f9a20
league <league@192.168.1.83>
2022-06-12 11:15:40 -0700

    gdfg

--- src/_05_typing_tutor/typing_tutor.java
+++ src/_05_typing_tutor/typing_tutor.java
@@ -1,74 +1,23 @@
 package _05_typing_tutor;
 
-public class typing_tutor {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+public class typing_tutor {
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	char currentLetter;
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	public void run() {
-		// TODO Auto-generated method stub
-		
+			
+	}
+		char gener ateRandomLetter() {
+		    Random r = new Random();
+		    return (char) (r.nextInt(26) + 'a');
+		}
+		currentLetter = generateRandomLetter();
+
 	}
 
 }
